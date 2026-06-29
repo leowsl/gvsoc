d315681f3b025c373743d7b344354a20a8221321
Leonard Wechsler <lwechsler@ethz.ch>
2026-06-29 12:00:33 +0000

    feat: barrier test application

---
+++ examples/SoftHier/software/barrier_test/main.c
@@ -0,0 +1,64 @@
+#include "flex_runtime.h"
+
+/// Test Timeout in nanoseconds
+#define TIMEOUT 1000000
+
+/// Evaluates to true on exactly one core of the system
+#define FIXED_CORE (flex_is_first_core() && (flex_get_cluster_id() == 0))
+
+/// Helper to run a test function and measure the execution time
+#define run_test(fn, arg) \
+    do { \
+        if (FIXED_CORE) printf("\n---------------------------------------------------------\n[Test Bench]: " #fn "(" #arg ")\n"); \
+        flex_global_barrier(); \
+        if (FIXED_CORE) flex_timer_start(); \
+        fn(arg); \
+        flex_global_barrier(); \
+        if (FIXED_CORE) { \
+            flex_timer_end(); \
+            printf("---------------------------------------------------------\n\n"); \
+        } \
+        flex_global_barrier(); \
+    } while(0) \
+
+
+/**
+ * @brief Test the global barrier `flex_global_barrier()`.
+ * @param iter  number of iterations
+ * @returns exitcode, 0 if test passed
+ */
+static int global_barrier(int iter) {
+    for (volatile int i = 0; i < iter; ++i) flex_global_barrier();
+    return 0;
+}
+
+/**
+ * @brief Test the xy barrier `flex_global_barrier_xy()`.
+ * @param iter  number of iterations
+ * @returns exitcode, 0 if test passed
+ */
+static int xy_barrier(int iter) {
+    for (volatile int i = 0; i < iter; ++i) flex_global_barrier_xy();
+    return 0;
+}
+
+int main()
+{
+    uint32_t eoc_val = 0;
+    flex_barrier_init();
+    flex_barrier_xy_init();
+    flex_sat(TIMEOUT);
+
+    // Global barrier tests    
+    run_test(global_barrier, 10);
+    run_test(global_barrier, 100);
+    run_test(global_barrier, 1000);
+    
+    // xy barrier tests
+    run_test(xy_barrier, 10);
+    run_test(xy_barrier, 100);
+    run_test(xy_barrier, 1000);
+
+    flex_eoc(eoc_val);
+    return 0;
+}
